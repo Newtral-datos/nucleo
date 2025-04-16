ef82f7e33ddc9353627e33e932b61ae1d8e38c7b
Newtral Data <visualizacion@newtral.es>
2025-04-16 13:30:24 +0200

    datos

---
+++ config.js
@@ -0,0 +1,165 @@
+var config = {
+    accessToken: 'pk.eyJ1IjoibmV3dHJhbCIsImEiOiJjazJrcDY4Y2gxMmg3M2JvazU4OXV6NHZqIn0.VO5GkvBq_PSJHvX7T8H9jQ',
+    showMarkers: true,
+    inset: true,
+    insetOptions: {
+        markerColor: 'orange'
+    },
+    insetPosition: 'bottom-right',
+    theme: 'dark',
+    use3dTerrain: false,
+    auto: false,
+    title: '',
+    subtitle: '',
+    byline: '',
+    footer: '',
+    chapters: [
+        {
+            id: 'slug-style-id',
+            alignment: 'right',
+            hidden: false,
+            title: 'Espacio Ardemans: presentación en público',
+            image: './assets/nn_1.PNG',
+            description: 'Núcleo Nacional se presentó en público el 14 de abril de 2024, coincidiendo con el Día de la República. Lo hizo en el Espacio Ardemans, un local de Madrid utilizado por organizaciones de extrema derecha como Falange, Facta, OHKA, Hacer Nación o la Hermandad Nacional de la División Azul. Su principal portavoz, que se identifica con el alias “Rana Baneada”, apeló a la unidad de “falangistas, franquistas, nacionalsocialistas y conservadores”.',
+            location: {
+                center: [-3.6754298564539813, 40.43682589398758],
+                zoom: 17,
+                pitch: 40,
+                bearing: 50,
+                speed: 1.0,
+                curve: 1.2
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'second-identifier',
+            alignment: 'right',
+            hidden: false,
+            title: 'Parque Juan Carlos I: entrenamientos al aire libre',
+            image: './assets/nn_2.PNG',
+            description: 'Varios vídeos de propaganda compartidos por los canales oficiales de Núcleo Nacional muestran a varios de sus integrantes entrenando deportes de contacto en el Parque Juan Carlos I de Madrid. Aquí también han grabado algunos comunicados públicos como el difundido el 10 de junio de 2024, donde su portavoz aseguró que España no tiene una “solución democrática” y animaba a los jóvenes a acercarse al grupo ultra en un acto previsto en la ciudad de Valencia.',
+            location: {
+                center: [-3.6048766753207837, 40.46037841508505],
+                zoom: 17,
+                pitch: 40,
+                bearing: 50,
+                speed: 1.0,
+                curve: 1.2
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'third-identifier',
+            alignment: 'left',
+            hidden: false,
+            title: '“El Alcázar” de Valladolid: la “sede”',
+            image: './assets/nn_3.png',
+            description: 'Cuatro meses después de su presentación en público, Núcleo Nacional se constituyó como asociación de ámbito estatal ante el Ministerio del Interior el 8 de agosto de 2024. La sede oficial que figura en los registros se encuentra en un local de Valladolid conocido como “El Alcázar”, lugar utilizado por varios colectivos ultra como el partido Democracia Nacional, del que fue líder uno de los actuales portavoces de Núcleo Nacional: Enrique Lemus.',
+            location: {
+                center: [-4.715584702716648, 41.652076864719604],
+                zoom: 17,
+                pitch: 40,
+                bearing: 50,
+                speed: 1.0,
+                curve: 1.2
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'fourth-chapter',
+            alignment: 'left',
+            hidden: false,
+            title: 'Pueblo Nuevo: el epicentro madrileño',
+            image: './assets/nn_4.png',
+            description: 'Aunque la sede oficial de Núcleo Nacional figura en Valladolid, la geolocalización de sus vídeos de propaganda permite identificar el barrio de Pueblo Nuevo como uno de los lugares más frecuentados por la organización. De hecho, en un radio de apenas 100 metros se concentran el gimnasio donde entrenan varios de sus miembros, la empresa que elabora y patrocina sus camisetas y el parque donde han grabado sus últimos videocomunicados.',
+            location: {
+                center: [-3.6314216086180466, 40.420793724752194],
+                zoom: 17,
+                pitch: 40,
+                bearing: 50,
+                speed: 1.0,
+                curve: 1.2
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'fifth-chapter',
+            alignment: 'left',
+            hidden: false,
+            title: 'El gimnasio',
+            image: './assets/nn_5.png',
+            description: 'El lugar utilizado por Núcleo Nacional para grabar algunos de sus vídeos de propaganda y para entrenar deportes de contacto es un gimnasio situado en la calle La Española, en el barrio de Pueblo Nuevo. Tal y como ha comprobado Newtral, aquí es donde se grabó el comunicado difundido el pasado 30 de marzo y por el que la Guardia Civil investiga al grupo ultra.',
+            location: {
+                center: [-3.6325691916344587, 40.42155678841447],
+                zoom: 17,
+                pitch: 40,
+                bearing: 50,
+                speed: 0.1,
+                curve: 2
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'sixth-chapter',
+            alignment: 'left',
+            hidden: false,
+            title: 'El Parque de La Almudena',
+            image: './assets/nn_6.png',
+            description: 'Núcleo Nacional graba parte de sus comunicados en el Parque de la Almudena, entre ellos el publicado el pasado 2 de abril. Este lugar está a menos de cien metros del gimnasio de la calle La Española, identificado como uno de los lugares donde entrenan varios de sus miembros.',
+            location: {
+                center: [-3.6337885842001443, 40.42081376421975],
+                zoom: 17,
+                pitch: 40,
+                bearing: 50,
+                speed: 0.1,
+                curve: 2
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'seventh-chapter',
+            alignment: 'left',
+            hidden: false,
+            title: 'El patrocinador',
+            image: './assets/nn_7.png',
+            description: 'La marca Sisebuto Division es la patrocinadora y distribuidora de las camisetas oficiales de Núcleo Nacional. Su sede está en el número 10 de la calle Villaescusa, a menos de cien metros del gimnasio. El propietario de esta empresa textil es Víctor Diego Villalba, militante de la extinguida Alianza Nacional y uno de los 14 condenados por el asalto violento a la librería Blanquerna en el año 2013.',
+            location: {
+                center: [-3.6329567243845897, 40.4223524988505],
+                zoom: 17,
+                pitch: 40,
+                bearing: 50,
+                speed: 0.1,
+                curve: 2
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    ]
+};
